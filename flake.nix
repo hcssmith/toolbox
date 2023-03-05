3eb96bef7080c24d13f48e167dcd4058819ace47
Halla Smith <me@hcssmith.com>
2023-03-05 23:38:30 +0000

    Minor refactor

--- flake.nix
+++ flake.nix
@@ -11,16 +11,7 @@
     pkgs = nixpkgs.legacyPackages.${system};
   in
   {
-    simple_web_server = pkgs.runCommand "simple_web_server" {
-      propogatedBuildInputs = [
-        pkgs.python3
-      ];
-    } ''
-      mkdir -p $out/bin
-      echo "${pkgs.python3}/bin/python3.10 -m http.server"  > $out/bin/simple_web_server
-      chmod +x $out/bin/simple_web_server
-'';
-    
+    simple_web_server = pkgs.callPackage ./simple_web_server { };
 
     packages.${system}.default = pkgs.writeScriptBin "test" ''
       echo Usage: nix run toolbox#tool
--- simple_web_server/default.nix
+++ simple_web_server/default.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}:
-pkgs.runCommand "simple_web_server" {} ''
-
-echo test
+pkgs.runCommand "simple_web_server" {
+      propogatedBuildInputs = [
+        pkgs.python3
+      ];
+    } ''
+      mkdir -p $out/bin
+      echo "${pkgs.python3}/bin/python3.10 -m http.server"  > $out/bin/simple_web_server
+      chmod +x $out/bin/simple_web_server
 ''
